48caeb7a563c21a0fa9da5ddeeeab26f88b3948f
Gabriel López Garcia <gabriellopezgarcia12@gmail.com>
2025-03-28 15:27:12 +0000

    Arreglado filtro de series/peliculas

--- js/PeliculasSeries.js
+++ js/PeliculasSeries.js
@@ -1,4 +1,10 @@
-import {cargarPeliculasSeries, cargarRecomendaciones, loadDefaultTemplates, loadTemplate} from "./script.js";
+import {
+    cargarPeliculasSeries,
+    cargarRecomendaciones,
+    loadDefaultTemplates,
+    loadTemplate,
+    moverElementosFiltro
+} from "./script.js";
 
 
 function addEventListenerBotonesPagina() {
@@ -17,11 +23,14 @@ async function initializePage() {
     loadTemplate("/templates/Buscador.html", 'buscador-contenedor');
     await cargarRecomendaciones('/templates/Pelicula_Sinopsis.html', "contenedor-peliculas", 'elementos_peliculas', 20); // Cargar las recomendaciones si es necesario
     /*loadTemplate('../templates/Contenedor_filtro.html', 'template-ajustes');*/
-    await cargarRecomendaciones('/templates/Contenedor_filtro.html', "template-ajustes", 'contenedor-filtro', 1); // Cargar las recomendaciones si es necesario
-    await cargarRecomendaciones('/templates/Label.html', "contenedor-filtro", 'elemento-filtro', 5); // Cargar las recomendaciones si es necesario
+    await cargarRecomendaciones('../templates/Contenedor_filtro.html', "template-filtros", 'contenedor-filtro', 1);
+    await cargarRecomendaciones('../templates/Label.html', "contenedor-filtro-1", 'elemento-filtro', 5);
     await cargarPeliculasSeries('elementos_peliculas', 20);
     addEventListenerBotonesPagina();
+    moverElementosFiltro()
 }
+
+
 
 
 window.onload = async () => initializePage();
