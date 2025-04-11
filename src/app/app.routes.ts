cd4dff193984d9a3372510156a04bb7c94088de5
Gabriel López Garcia <gabriellopezgarcia12@gmail.com>
2025-04-11 16:17:30 +0100

    Hecho descubrir

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import {MainComponent} from './main/main.component';
 import {PeliculasSeriesComponent} from './peliculas-series/peliculas-series.component';
+import {DescubrirComponent} from './descubrir/descubrir.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: MainComponent},
-  {path: 'peliculas-series', component: PeliculasSeriesComponent}
+  {path: 'peliculas-series', component: PeliculasSeriesComponent},
+  {path: 'descubrir', component: DescubrirComponent}
+
+
 ];
