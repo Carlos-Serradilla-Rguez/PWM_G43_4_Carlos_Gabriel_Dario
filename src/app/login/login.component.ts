d87d47d240b97fcc33bd95e78ff19c3f10c9d2ac
Carlos Serradilla Rguez <carlos.serradilla101@alu.ulpgc.es>
2025-04-18 12:05:01 +0100

    Cambio de botón a link

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 
 
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
