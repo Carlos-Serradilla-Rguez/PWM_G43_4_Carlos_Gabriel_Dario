3a4d3a17b2ef2cffd1668aed221cfce739f4d9fb
xdarlo <dario.acuna101@alu.ulpgc.es>
2025-04-23 11:55:02 +0200

    Pagina de inicio hecha. Se cargan las imagenes de las peliculas y series con el tamaño adecuado

--- src/app/main/mas-vistos/mas-vistos.component.ts
+++ src/app/main/mas-vistos/mas-vistos.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-import {PortadaComponent} from '../../Shared/portada/portada.component';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PortadaComponent } from '../../Shared/portada/portada.component';
+import { SeriesService } from '../../core/services/series.service';
 
 @Component({
   selector: 'app-mas-vistos',
-  imports: [PortadaComponent],
+  standalone: true,
+  imports: [PortadaComponent,CommonModule],
   templateUrl: './mas-vistos.component.html',
   styleUrl: './mas-vistos.component.css'
 })
 export class MasVistosComponent {
+  series: any[] = [];
+  private seriesService = inject(SeriesService);
 
+  async ngOnInit() {
+    this.series = await this.seriesService.getRandomSeries(4);
+  }
 }
+
