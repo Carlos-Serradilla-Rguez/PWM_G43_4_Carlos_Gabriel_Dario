cd131e602deea4a8d4bcdb0aa12a3f4c70756839
Carlos Serradilla <carlos.serradilla101@alu.ulpgc.es>
2025-04-23 09:34:38 +0100

    Me traigo la portada.

--- src/app/Shared/portada/portada.component.ts
+++ src/app/Shared/portada/portada.component.ts
@@ -1,16 +1,28 @@
-import { Component, Input} from '@angular/core';
-import {NgOptimizedImage} from '@angular/common';
+import {Component, Input} from '@angular/core';
+import {NgIf, NgOptimizedImage} from '@angular/common';
 import {RouterLink} from '@angular/router';
+import {Serie} from '../../core/services/series.service';
+import {Actor} from '../../core/services/actores.service';
 
 @Component({
   selector: 'app-portada',
   imports: [
     NgOptimizedImage,
-    RouterLink
+    RouterLink,
+    NgIf
   ],
   templateUrl: './portada.component.html',
   styleUrl: './portada.component.css'
 })
 export class PortadaComponent {
-  @Input() serie: any;
+  @Input() item: Serie | Actor | null = null;
+
+  isSerie(item: Serie | Actor | null): item is Serie {
+    return (item as Serie)?.portada !== undefined;
+  }
+
+  // Type guard para verificar si es un Actor
+  isActor(item: Serie | Actor | null): item is Actor {
+    return (item as Actor)?.foto !== undefined;
+  }
 }
